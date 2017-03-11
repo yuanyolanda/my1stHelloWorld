a40cfd892de2c118dbb0419331567d6b0c612b99
yuanyolanda <yuanyolanda0108@gmail.com>
2017-03-11 16:16:03 +0800

    first commit

---
+++ Hello World/2ndVCViewController.swift
@@ -0,0 +1,35 @@
+//
+//  2ndVCViewController.swift
+//  Hello World
+//
+//  Created by Ray on 11/3/2017.
+//  Copyright © 2017 Ray. All rights reserved.
+//
+
+import UIKit
+
+class _ndVCViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
